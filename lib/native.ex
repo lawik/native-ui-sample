be1c505e5fd53420f1c9a2659806d72732feb329
Lars Wikman <lars@underjord.io>
2021-07-23 10:30:21 +0200

    Add code editing widget and do some change tracking, unfinished

--- lib/native.ex
+++ lib/native.ex
@@ -25,9 +25,15 @@ defmodule Native do
     text_id = System.unique_integer([:positive, :monotonic])
     text = :wxTextCtrl.new(panel, text_id, pos: {0, 32})
 
+    editor_id = System.unique_integer([:positive, :monotonic])
+    editor = :wxStyledTextCtrl.new(panel, id: editor_id, pos: {0, 72})
+    #:wxEvtHandler.connect(editor, :stc_change, id: editor_id)
+    :wxEvtHandler.connect(editor, :stc_modified, id: editor_id)
+    #:wxEvtHandler.connect(editor, :stc_charadded, id: editor_id)
+
     :wxFrame.show(frame)
 
-    state = %{panel: panel, frame: frame, button: button, text: text}
+    state = %{panel: panel, frame: frame, button: button, text: text, editor: editor, delta: []}
     {frame, state}
   end
 
@@ -44,11 +50,53 @@ defmodule Native do
     # :wxButton.destroy(ref)
     text_line = :wxTextCtrl.getLineText(state.text, 0)
     :wxButton.setLabel(state.button, text_line)
+    text = :wxStyledTextCtrl.getText(state.editor)
+    IO.puts(to_string(text))
     {:noreply, state}
   end
 
+  def handle_event({:wx, _, ref, _, {:wxStyledText, :stc_modified, pos, _, _, flags, text, length, _, _, _, _, _, _, _, _, _, _, _, _, _, _}}, %{delta: delta} = state) do
+    op = case <<flags>> do
+      # Insert, 0x01
+      <<_::7, 1::1>> <> _ ->
+        :insert
+      # Delete, 0x02
+      <<_::6, 1::1, 0::1>> <> _ ->
+        :delete
+      _bin_flags ->
+        # <<i1::1, i2::1, i3::1, i4::1, i5::1, i6::1, i7::1, i8::1>> = bin_flags
+        # IO.inspect({i1, i2, i3, i4, i5, i6, i7, i8})
+        # IO.inspect(byte_size(bin_flags))
+        # IO.inspect(bin_flags)
+        # IO.inspect({flags, as_binary(flags)})
+        :nothing
+    end
+
+    case {op, delta} do
+      # More of the same operation, add them together
+      {op, [%{op: ^op} = latest | _]} -> %{latest | }
+      # No operation started, start it
+      {op, []} -> %{}
+      # Change of operations, start a new one
+    end
+
+    {:noreply, state}
+  end
+
+  # # Insert text
+  # def handle_event({:wx, _, ref, _, {:wxStyledText, :stc_modified, pos, _, _, <<_::1,1::1,_::6>> <> _flags, text, length, _, _, _, _, _, _, _, _, _, _, _, _, _, _}}, state) do
+  #   IO.inspect({"insert", pos, length, text})
+  #   {:noreply, state}
+  # end
+
+  # # Delete text
+  # def handle_event({:wx, _, ref, _, {:wxStyledText, :stc_modified, pos, _, _, <<_::1,_::1,1::1,_::5>> <> _flags, text, length, _, _, _, _, _, _, _, _, _, _, _, _, _, _}}, state) do
+  #   IO.inspect({"delete", pos, length, text})
+  #   {:noreply, state}
+  # end
+
   def handle_event({:wx, _, _, _, evt}, state) do
-    IO.inspect(evt, label: "Event")
+    IO.inspect({evt, System.unique_integer([:positive, :monotonic])}, label: "Event")
     {:noreply, state}
   end
 
@@ -61,5 +109,9 @@ defmodule Native do
     :wxDC.clear(dc)
     :wxPaintDC.destroy(dc)
     :ok
+  end
+
+  def as_binary(num) do
+    List.to_string(:io_lib.format("~8.2B", [num]))
   end
 end
